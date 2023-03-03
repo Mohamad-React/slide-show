965c5c3043058f9b36cded9c9f6100e8d1d0a98a
mohammad razavi <mohammadrazavi71@gmail.com>
2023-03-03 20:56:52 +0330

    start slide show

---
+++ src/Components/data.js
@@ -0,0 +1,27 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'mohammad razavi',
+        job: "user interface",
+        image: 'https://images.pexels.com/photos/9154716/pexels-photo-9154716.jpeg?cs=srgb&dl=pexels-cottonbro-9154716.jpg&fm=jpg',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+
+    },
+    {
+        id: 2,
+        name: "jafar panahi",
+        job: "gerafist",
+        image: 'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo"
+    },
+    {
+        id: 3,
+        name: "golsa hooshmand",
+        job: "programmiere",
+        image: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        text: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt"
+    }
+]
+
+
+export default reviews;
